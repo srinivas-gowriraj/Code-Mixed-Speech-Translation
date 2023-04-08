92128823eacfeab46a12fe324981307591fbea2a
sohamtiwari3120 <soham.tiwari800@gmail.com>
2023-04-08 12:44:56 -0400

    adding commit file

---
+++ config.py
@@ -0,0 +1,5 @@
+class hparams:
+    def __init__(self):
+        self.cmst_data_path = "/usr0/home/sohamdit/11692/Code-Mixed-Speech-Translation/data/cmst/Speech Data"
+        self.sample_rate = 16000
+        self.test_size = 0.2
